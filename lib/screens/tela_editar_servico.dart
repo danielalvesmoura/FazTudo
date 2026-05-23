8b8abf61c5ce77ee3d9a57d634d878b0e0471f0e
danielalvesmoura <danielalvesmoura2006@gmail.com>
2026-05-23 11:13:32 -0300

    feat: continuando tela

--- lib/screens/tela_editar_servico.dart
+++ lib/screens/tela_editar_servico.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/servico.dart';
 import 'package:flutter_application_1/widgets/botao_flutuante.dart';
 import 'package:flutter_application_1/widgets/botao_voltar.dart';
 import 'package:flutter_application_1/widgets/campo.dart';
@@ -8,7 +9,7 @@ import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 enum categorias { nenhuma, consertos, limpeza, beleza, transporte, alimentacao }
 
 class TelaEditarServico extends StatefulWidget {
-  final servico;
+  final Servico servico;
 
   TelaEditarServico({required this.servico});
 
@@ -35,97 +36,125 @@ class TelaEditarServicoState extends State<TelaEditarServico> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    campoTitulo.text = widget.servico.titulo;
+    campoCep.text = widget.servico.cep;
+    campoDescricao.text = widget.servico.descricao;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 50),
-            child: Column(
+        Column(
+          children: [
+            Row(
               children: [
-                SizedBox(height: 20),
-
-                Text(
-                  'Conte-nos os detalhes',
-                  style: TextStyle(
-                    fontSize: 40, 
-                    fontWeight: FontWeight.w700,
-                    decoration: TextDecoration.none,
-                    color: Colors.black
-                  ),
-                ),
-          
-                SizedBox(height: 20),
-          
-                Text(
-                  'Descreva bem o que você oferece e onde atende.',
-                  style: TextStyle(
-                    fontSize: 20, 
-                    fontWeight: FontWeight.w500,
-                    decoration: TextDecoration.none,
-                    color: const Color.fromARGB(255, 59, 59, 59)
-                  ),
-                ),
-          
-                SizedBox(height: 60),
-          
-                Campo(
-                  label: 'Título do Serviço', 
-                  hint: 'Ex: Troca de Pneus e óleo', 
-                  icon: null,
-                  minLines: 1,
-                  maxLines: 2,
-                  controller: campoTitulo,
-                ),
-
-                SizedBox(height: 30),
-
-                Campo(
-                  label: 'Descrição Detalhada', 
-                  hint: 'Explique como funciona seu serviço...', 
-                  icon: null,
-                  minLines: 1,
-                  maxLines: 20,
-                  controller: campoDescricao,
-                ),
-
-                SizedBox(height: 30),
-
-                Campo(
-                  label: 'CEP', 
-                  hint: '00000-000', 
-                  icon: null,
-                  minLines: 1,
-                  maxLines: 20,
-                  formatters: [cepFormatter],
-                  controller: campoCep,
-                ),
-
-                SizedBox(height: 30),
-
-                Row(
-                  children: [
-                    Text(
-                      'Subcategoria',
-                      style: TextStyle(
-                        color: Color.fromARGB(255, 65, 65, 65),
-                        decoration: TextDecoration.none,
-                        fontSize: 17,
-                        fontWeight: FontWeight.w700
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 20,),
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: SubcategoriasConsertoCadastro(),
-                ),
+                BotaoVoltar(),
                 
-                SizedBox(height: 188),
+                Text(
+                  "Editar Serviço",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 36, 56, 155),
+                    decoration: TextDecoration.none,
+                    fontSize: 25,
+                    fontWeight: FontWeight.w600
+                  ),
+                  textAlign: TextAlign.center,
+                ),
               ],
             ),
-          ),
+
+            Stack(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(widget.servico.url),
+                      fit: BoxFit.cover
+                    )
+                  ),
+                
+                  height: 300,
+                ),
+                SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 200),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(40)
+                      ),
+                      child: Padding(
+                        padding: EdgeInsetsGeometry.symmetric(horizontal: 30),
+                        child: Column(
+                          children: [  
+                        
+                            SizedBox(height: 30),
+                        
+                            Campo(
+                              label: 'Título do Serviço', 
+                              hint: 'Ex: Troca de Pneus e óleo', 
+                              icon: null,
+                              minLines: 1,
+                              maxLines: 2,
+                              controller: campoTitulo,
+                            ),
+                                        
+                            SizedBox(height: 30),
+                                        
+                            Campo(
+                              label: 'Descrição Detalhada', 
+                              hint: 'Explique como funciona seu serviço...', 
+                              icon: null,
+                              minLines: 1,
+                              maxLines: 20,
+                              controller: campoDescricao,
+                            ),
+                                        
+                            SizedBox(height: 30),
+                                        
+                            Campo(
+                              label: 'CEP', 
+                              hint: '00000-000', 
+                              icon: null,
+                              minLines: 1,
+                              maxLines: 20,
+                              formatters: [cepFormatter],
+                              controller: campoCep,
+                            ),
+                                        
+                            SizedBox(height: 30),
+                                        
+                            Row(
+                              children: [
+                                Text(
+                                  'Subcategoria',
+                                  style: TextStyle(
+                                    color: Color.fromARGB(255, 65, 65, 65),
+                                    decoration: TextDecoration.none,
+                                    fontSize: 17,
+                                    fontWeight: FontWeight.w700
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 20,),
+                            SingleChildScrollView(
+                              scrollDirection: Axis.horizontal,
+                              child: SubcategoriasConsertoCadastro(),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            )
+            
+          ],
         ),
 
         BotaoFlutuante(
@@ -138,22 +167,7 @@ class TelaEditarServicoState extends State<TelaEditarServico> {
           enabled: true,
         ),
 
-        Row(
-          children: [
-            BotaoVoltar(),
-            
-            Text(
-              "Editar Serviço",
-              style: TextStyle(
-                color: Color.fromARGB(255, 36, 56, 155),
-                decoration: TextDecoration.none,
-                fontSize: 25,
-                fontWeight: FontWeight.w600
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ],
-        ),
+        
       ],
     );
   }
